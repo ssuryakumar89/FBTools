0594939089d6f8a3eb40f3822a6b8109ee690e30
Surya Kumar <ur2cdanger@gmail.com>
2018-04-13 16:23:58 -0700

    Created template file
    Just as a reference

---
+++ script.py
@@ -0,0 +1,42 @@
+# Use python 3 
+# Just a skeleton 
+# define a class like this 
+
+class FBTools:
+
+  def _int_():
+      #function body 
+      
+  def login(username, password):
+  
+  
+  def start_bday_wish_deamon():
+  
+  
+  def get_friends_list():
+  
+  
+  def get_friend_location():
+  
+  
+  def get_all_friends_locations():
+  
+  
+  def get_friends_birthdays():
+  
+  
+  def get_friends_contacts():
+  
+  
+  def get_friends_phone():
+  
+  
+  def get_friends_email():
+  
+  
+# Then use it this way
+
+FBTools f
+f.login(username,password)
+f.start_wish_deamon()
+  
